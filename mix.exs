fb044c17eb4b7be93b56e72c01994a7344a5cdc9
Erion S <47327807+SOVRON@users.noreply.github.com>
2023-03-19 19:06:58 +0000

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -5,14 +5,16 @@ defmodule Postgrex.Mixfile do
     [
       app: :phoenix_channel_with_fallback,
       version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
       elixir: "~> 1.14",
-      build_embedded: Mix.env == :prod,
-      start_permanent: Mix.env == :prod,
-      description: "Response fallback for Phoenix channel functions",
-      package: package(),
-      deps: deps(),
-      name: "Phoenix Channel With Fallback",
-      source_url: "https://github.com/par8o/phoenix_channel_with_fallback"
+      elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
+      deps: deps()
+      source_url: "https://github.com/SOVRON/phoenix_channel_with_fallback"
     ]
   end
 
